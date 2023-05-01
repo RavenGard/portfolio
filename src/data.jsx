f91c95a70101138684a1d3b94018107a331d8119
RavenGard <101682799+RavenGard@users.noreply.github.com>
2023-05-01 18:06:04 -0400

    Deploy

---
+++ src/data.jsx
@@ -0,0 +1,39 @@
+export const projects = [
+  {
+    title: "SoftIQ",
+    subtitle: "MERN Stack & GraphQL",
+    description:
+      "Web application simulating a mock interview environment.",
+    image: "./softiq.gif",
+    link: "https://github.com/RavenGard/SoftIQ",
+  },
+  {
+    title: "QuicKart",
+    subtitle: "JavaScript, React, MongoDB, & GraphQL",
+    description:
+      "This app enables users to enter a series of items and generate the most efficient shopping route.",
+    image: "./quickart.gif",
+    link: "https://github.com/RavenGard/QuicKart",
+  },
+  {
+    title: "Text-based Adventure Game",
+    subtitle: "Java",
+    description:
+    "A game where the player navigates rooms to solve puzzles, interact with items, and fight monsters.",
+    image: "./jl2022.gif",
+    link: "https://github.com/RavenGard/JusticeLeague-Spring2022",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "Java",
+  "React & React Native",
+  "HTML/CSS",
+  "Node",
+  "GraphQL",
+  "Python",
+  "PHP",
+  "MySQL",
+  "MongoDB"
+];
